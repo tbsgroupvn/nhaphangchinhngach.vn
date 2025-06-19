713cad432b4a3c2d30a526522fbb8a5be3a01ed6
TBS Group <tbsgroup.work@gmail.com>
2025-06-19 17:00:04 +0700

    1
    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import ZaloQR from '@/components/ZaloQR'
-import MiniChatbot from '@/components/MiniChatbot'
-import AnalyticsScripts from '@/components/AnalyticsScripts'
+import ZaloQR from '../components/ZaloQR'
+import MiniChatbot from '../components/MiniChatbot'
+import AnalyticsScripts from '../components/AnalyticsScripts'
 import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
